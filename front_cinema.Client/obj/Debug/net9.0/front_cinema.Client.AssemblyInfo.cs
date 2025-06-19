2671159fc932b0fb4b7e1928f27728f1dfe2090e
Welliton Dev <gioriwelliton47@gmail.com>
2025-06-18 22:30:39 -0400

    feat: data filter sessao

--- front_cinema.Client/obj/Debug/net9.0/front_cinema.Client.AssemblyInfo.cs
+++ front_cinema.Client/obj/Debug/net9.0/front_cinema.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("front_cinema.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cb2fb17be42516d504401510a4852a4e57a832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a758dc36041d9298c1d4edf1734e567e16eb8114")]
 [assembly: System.Reflection.AssemblyProductAttribute("front_cinema.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("front_cinema.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
